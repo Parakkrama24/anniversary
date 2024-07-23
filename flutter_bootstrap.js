d45332e2a96d46cbfe8580527176b207fbc1b316
Parakkrama24 <Parakkrama24@users.noreply.github.com>
2024-07-23 07:35:05 +0000

    deploy: bbeca8c213d2ef68dc3defce6f7f2e3d9f108e32

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "274669846"
+    serviceWorkerVersion: "1492463750"
   }
 });
